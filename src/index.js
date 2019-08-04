5ef1269247d6aa644c60458355375f346e6c96ce
sample <dorothy_007@naver.com>
2019-08-05 01:42:38 +0900

    install babel

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'mobx-react'; // MobX 에서 사용하는 Provider
+import { Provider } from 'mobx-react';
 import App from './App';
-import CounterStore from './stores/counter'; // 방금 만든 스토어 불러와줍니다.
+import CounterStore from './stores/counter';
 
-const counter = new CounterStore(); // 스토어 인스턴스를 만들고
+const counter = new CounterStore();
 
 ReactDOM.render(
   <Provider counter={counter}>
-    {/* Provider 에 props 로 넣어줍니다. */}
     <App />
   </Provider>,
   document.getElementById('root')
